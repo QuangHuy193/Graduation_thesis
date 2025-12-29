d8f758df5567c8a6935c783693c7cff10278c31a
QuangHuy193 <quanghuylhpq@gmail.com>
2025-12-29 10:44:13 +0700

    fixed lỗi 402 (2)

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,3 @@
-
 // app/admin/page.tsx  (SERVER)
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth/next";
@@ -8,18 +7,18 @@ import AdminClient from "./AdminClient";
 export const dynamic = "force-dynamic"; // đảm bảo server-side
 
 export default async function AdminPage() {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    // chưa login -> redirect login (với callback)
-    if (!session) {
-        redirect(`/login?callbackUrl=/admin`);
-    }
+  // chưa login -> redirect login (với callback)
+  if (!session) {
+    redirect(`/login?callbackUrl=/admin`);
+  }
 
-    // có session nhưng không phải admin -> 403
-    const role = (session as any).user?.role ?? null;
-    if (role !== "admin") {
-        redirect("/403");
-    }
+  // có session nhưng không phải admin -> 403
+  const role = (session as any).user?.role ?? null;
+  if (role !== "admin") {
+    redirect("/403");
+  }
 
-    return <AdminClient />;
+  return <AdminClient />;
 }
